14eb47e97ed26ad3026810aca6cbdbd54951c015
thegreatpl <paulmarkwhite@hotmail.co.uk>
2025-01-25 06:14:48 +0000

    Structure saving basics done.

---
+++ Assets/Editor/StructureHandler.cs
@@ -0,0 +1,85 @@
+using System.IO;
+using System.Runtime.InteropServices;
+using UnityEditor;
+using UnityEngine;
+
+public class StructureHandler : MonoBehaviour
+{
+    [MenuItem("MapGeneration/ClearMap")]
+    public static void ClearMap()
+    {
+        var map = SceneAsset.FindAnyObjectByType<MapController>();
+        map.Foreground.ClearAllTiles();
+        map.BackGround.ClearAllTiles();
+        map.Walls.ClearAllTiles();
+    }
+
+
+    [MenuItem("MapGeneration/Save Structure")]
+    public static void SaveStructure()
+    {
+        var structure = new StructureDef();
+        structure.name = "testing"; 
+
+        structure.tiles = new System.Collections.Generic.List<StructureTile>(); 
+
+        var map = SceneAsset.FindAnyObjectByType<MapController>();
+
+
+        var foreBounds = map.Foreground.cellBounds;
+        var wallBounds = map.Walls.cellBounds;
+        var backBounds = map.BackGround.cellBounds;
+        int minX, minY, maxX, maxY;
+        minX = foreBounds.xMin;
+        minY = foreBounds.yMin;
+        maxX = foreBounds.xMax;
+        maxY = foreBounds.yMax; 
+
+        if (minX > wallBounds.xMin) minX = wallBounds.xMin;
+        if (minX > backBounds.xMin) minX = backBounds.xMin;
+        if (minY > wallBounds.yMin) minY = wallBounds.yMin;
+        if (minY > backBounds.yMin) minY = backBounds.yMin;
+        if (maxY < backBounds.yMax) maxY = backBounds.yMax;
+        if (maxY < wallBounds.yMax) maxY = wallBounds.yMax;
+        if (maxX < wallBounds.xMax) maxX = wallBounds.xMax;
+        if (maxX < backBounds.xMax) maxX = backBounds.xMax;
+
+        var bounds = new BoundsInt(minX, minY, foreBounds.zMin, maxX - minX, maxY - minY, foreBounds.zMax - foreBounds.zMin);
+
+        var foregroundTiles = map.Foreground.GetTilesBlock(bounds);
+        var wallTiles = map.Walls.GetTilesBlock(bounds);
+        var backTiles = map.BackGround.GetTilesBlock(bounds);
+
+        for (int xdx = 0; xdx < bounds.size.x; xdx++)
+            for (int ydx = 0; ydx < bounds.size.y; ydx++)
+            {
+                var foregroundtile = foregroundTiles[xdx + ydx * bounds.size.x]; 
+                var wallTile = wallTiles[xdx + ydx * bounds.size.x];
+                var backTile = backTiles[xdx + ydx * bounds.size.x];
+
+                if (foregroundtile != null)
+                    structure.tiles.Add(new StructureTile()
+                    {
+                        x = xdx,
+                        y = ydx,
+                        tilename = foregroundtile.name, 
+                        Layer = "Foreground"
+                    });
+                if (wallTile != null)
+                    structure.tiles.Add(new StructureTile()
+                    {
+                        x = xdx,
+                        y = ydx ,
+                        tilename = wallTile.name, 
+                        Layer = "wall"
+                    }); 
+                if (backTile != null)
+                    structure.tiles.Add(new StructureTile() { x = xdx, y = ydx , tilename = backTile.name, Layer = "background" });
+            }
+
+
+        var json = EditorJsonUtility.ToJson(structure);
+
+        File.WriteAllText($"Assets/Resources/Structures/{structure.name}.json", json);
+    }
+}
---
+++ Assets/Scripts/Map/StructureDef.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[Serializable]
+public class StructureDef
+{
+    public string name; 
+
+
+    public List<StructureTile> tiles;
+
+}
+
+
+
+[Serializable]
+public class StructureTile
+{
+    public string tilename;
+
+    public string Layer; 
+
+    public int x; 
+
+    public int y;
+}
