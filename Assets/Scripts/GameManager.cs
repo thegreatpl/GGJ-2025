c83cf7feaaebf034f10546a791cb2c38bf88f473
thegreatpl <paulmarkwhite@hotmail.co.uk>
2025-01-25 20:12:54 +0000

    Basic main menu done.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
     public static GameManager instance;
 
+    public Camera Camera; 
 
     public TileManager TileManager; 
 
@@ -16,6 +18,8 @@ public class GameManager : MonoBehaviour
     public PrefabManager PrefabManager;
 
     public StructureManager StructureManager;
+
+    public UIController UIController;
 
 
     public GameObject Player; 
@@ -33,9 +37,14 @@ public class GameManager : MonoBehaviour
         TileManager = GetComponent<TileManager>();
         WorldGenerator = GetComponent<WorldGenerator>();
         PrefabManager = GetComponent<PrefabManager>();
-        StructureManager = GetComponent<StructureManager>();    
+        StructureManager = GetComponent<StructureManager>();
+        UIController = GetComponentInChildren<UIController>();
 
         DontDestroyOnLoad(gameObject);
+        if (Camera == null)
+            Camera = Camera.main;
+
+        DontDestroyOnLoad(Camera);
 
         StartCoroutine(LoadGame()); 
     }
@@ -49,17 +58,75 @@ public class GameManager : MonoBehaviour
 
     IEnumerator LoadGame()
     {
+        UIController.HideInfoScreen();
+        UIController.SetGameUI(false);
         yield return StartCoroutine(TileManager.LoadTiles()); 
         yield return StartCoroutine(StructureManager.LoadStructures());
 
-        yield return StartCoroutine(StartNewGame()); 
+        //yield return StartCoroutine(StartNewGame("Tiffany")); 
     }
 
-    IEnumerator StartNewGame()
+    public void StartGame(string character)
     {
+        instance.StartCoroutine(StartNewGame(character));
+    }
+
+    IEnumerator StartNewGame(string Character)
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+
+        yield return null;       
+        UIController.ShowMessage("Loading..."); 
         MapController = FindAnyObjectByType<MapController>();//there should only be one so... 
         WorldGenerator.tileManager = TileManager;
         WorldGenerator.map = MapController;
         yield return StartCoroutine(WorldGenerator.GenerateWorld());
+        var characterPrefab = PrefabManager.GetPrefab(Character);
+        if (characterPrefab == null)
+        {
+            UIController.SendMessage($"ERROR: Character {Character} not found!");
+            StartCoroutine(EndGameWaitForInput());
+        }
+        Player = Instantiate(characterPrefab);
+        Camera.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, Camera.transform.position.z);
+        Camera.transform.SetParent(Player.transform);
+        yield return null; 
+        UIController.HideInfoScreen();
+        UIController.SetGameUI(true);
+    }
+
+    public void Escape()
+    {
+        if (Player == null)
+            //wtf this should not happen. 
+            return;
+
+        var attributes = Player.GetComponent<Attributes>();
+        int gold = attributes.Gold;
+        Player.transform.DetachChildren(); 
+        Destroy(Player);
+        UIController.ShowMessage($"You have escaped from the bubble worlds, carrying a fortune in gold. Earned: {gold}gp");
+        StartCoroutine(EndGameWaitForInput());
+
+    }
+
+    public void PlayerDeath()
+    {
+        Player.transform.DetachChildren(); 
+        Camera.transform.parent = null; 
+        Destroy(Player);
+        UIController.ShowMessage("Death... You failed to escape the bubble worlds"); 
+        StartCoroutine(EndGameWaitForInput());
+    }
+
+    IEnumerator EndGameWaitForInput()
+    {
+        if (Input.anyKeyDown)
+        {
+            UIController.HideInfoScreen();
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single); 
+        }
+        else
+            yield return null;
     }
 }
--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -1,16 +1,43 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class UIController : MonoBehaviour
 {
+    public GameObject InfoScreen;
+
+    public TextMeshProUGUI InfoText;
+
+
+    public GameObject GameUI; 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        HideInfoScreen();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void SetGameUI(bool gameUI)
+    {
+        GameUI.SetActive(gameUI);
+    }
+
+    public void HideInfoScreen()
+    {
+        InfoScreen.SetActive(false);
+    }
+
+
+    public void ShowMessage(string message)
+    {
+        InfoScreen.SetActive(true);
+        InfoText.text = message;
     }
 }
